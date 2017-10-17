4d6d86be8bc9b0fad751a1cfa866fe5b757e7bc3
BRENDA <BRENDA@DESKTOP-7O4U7A5>
2017-10-16 19:34:54 -0500

    Pract6

---
+++ Pract6/Pract6/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using SQLite;
+
+namespace Pract6
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            SQLiteAsyncConnection database;
+            string db;
+            db = DependencyService.Get<ISQLite>().GetLocalFilePath("Practica.db");
+            database = new SQLiteAsyncConnection(db);
+            database.CreateTableAsync<TESHDatos>().Wait();
+            var elemento = new TESHDatos
+            {
+                Matricula = "13090418",
+                Nombre = "Brenda Itzel",
+                Apellido = "Villanueva",
+                Direccion = "Cerrada Rio Bravo",
+                Telefono = "5517479951",
+                Carrera = "Ing. Sistemas Computacionales",
+                Semestre = "9",
+                Correo = "bren.villanueva10@gmail.com",
+                GitHub = "BrendaVillanueva"
+
+            };
+            database.InsertAsync(elemento);
+
+
+        }
+    }
+}
---
+++ Pract6/Pract6/TESHDatos.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+
+namespace Pract6
+{
+    [Table("Datos_Formulario")]
+    public class TESHDatos
+    {
+            string matricula; // int matricula
+            string nombre; 
+            string apellido; 
+            string direccion; 
+            string tel; 
+            string carrera;
+            string semestre;
+            string correo; 
+            string github; 
+
+
+            [PrimaryKey, Column("Matricula"), MaxLength(8)]
+            public string Matricula
+            {
+                      
+
+                get { return matricula; }
+                set { matricula = value; }
+            }
+
+            [MaxLength(30)]
+            public string Nombre
+            {
+                get { return nombre; }
+                set { nombre = value; }
+            }
+
+            [MaxLength(30)]
+            public string Apellido
+            {
+                get { return apellido; }
+                set { apellido = value; }
+            }
+         
+            [MaxLength(50)]
+            public string Direccion
+            {
+                get { return direccion; }
+                set { direccion = value; }
+            }
+
+           [MaxLength(10)]
+            public string Telefono
+            {
+                get { return tel; }
+                set { tel = value; }
+            }
+            
+        [MaxLength(40)]
+            public string Carrera
+            {
+                get { return carrera; }
+                set { carrera = value; }
+            }
+
+            [MaxLength(2)]
+            public string Semestre
+            {
+                get { return semestre; }
+                set { semestre = value; }
+            }
+            
+         [MaxLength(30)]
+            public string Correo
+            {
+                get { return correo; }
+                set { correo = value; }
+            }
+
+            [MaxLength(20)]
+            public string GitHub
+            {
+                get { return github; }
+                set { github = value; }
+            }
+        }
+
+    }
+
